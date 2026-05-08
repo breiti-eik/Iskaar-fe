23ea333d1136596dcbea2733cd7b2b5e608dac4d
Thomas Breitenbach <breiti-eik@gmx.de>
2026-05-08 04:57:01 +0200

    add StackView to Bank

--- iskaar-frontend/src/game/ui/BankView.ts
+++ iskaar-frontend/src/game/ui/BankView.ts
@@ -1,28 +1,40 @@
 import type { BankViewData } from "../view/BankViewData";
+import { StackView } from "./StackView";
 
 export class BankView extends Phaser.GameObjects.Container {
   private debugBg!: Phaser.GameObjects.Rectangle;
-  private bankViewData!: BankViewData;
+  private stack!: StackView;
+  private bankData!: BankViewData;
 
   constructor(scene: Phaser.Scene) {
     super(scene);
     this.scene.add.existing(this);
-
     this.setVisible(false);
     this.debugBg = this.scene.add.rectangle(0, 0, 100, 100, 0xff5500, 0);
     this.debugBg.setOrigin(0, 1);
 
-    this.add([this.debugBg]);
+    this.create();
   }
 
-  setBankData(victoryPoints: BankViewData) {
-    this.bankViewData = victoryPoints;
+  private create() {
+    // 🪦 Stack (mit Hover Expand!)
+    this.stack = new StackView(this.scene, 0.8, true, true, false);
+    this.stack.setScale(0.65);
+
+    this.add([this.debugBg, this.stack]);
+  }
+
+  setBankData(bankData: BankViewData) {
+    this.bankData = bankData;
+    this.stack.setCards(bankData.cards);
   }
 
   updateLayout(x: number, y: number, width: number, height: number) {
-    this.debugBg.setPosition(x, y);
+    this.setPosition(x, y);
     this.debugBg.setDisplaySize(width, height);
 
-    this.setVisible(true);
+    this.stack.setPosition(width * 0.6, -height / 2);
+
+    this.setVisible(this.bankData.open);
   }
 }
